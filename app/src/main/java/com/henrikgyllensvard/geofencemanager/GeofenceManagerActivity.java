5cff85e4ab10de4f4306c4e8ea26704bc91e0a7a
HGyllensvard <Henrik.Gyllensvard@gmail.com>
2016-11-21 23:00:00 +0000

    Use novoda release plugin to upload to bintray.
    Renamed MainActivity to GeofenceManagerActivity

--- app/src/main/java/com/henrikgyllensvard/geofencemanager/GeofenceManagerActivity.java
+++ app/src/main/java/com/henrikgyllensvard/geofencemanager/GeofenceManagerActivity.java
@@ -3,7 +3,7 @@ package com.henrikgyllensvard.geofencemanager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class GeofenceManagerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
